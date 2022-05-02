ecb4b7b1f854fcaf5c5e91992d1d16c2f509a6fc
SouabeRaphael <R.souabe03@gmail.com>
2022-05-02 12:56:58 +0200

    avancement

--- td_calculatrice/assets/js/app.js
+++ td_calculatrice/assets/js/app.js
@@ -9,9 +9,14 @@ let app = new Vue({
         operationmemory: '',
     },
     methods: {
+        // Fonction prenant en paramètre le charactère sélectioné par l'utilisateur et le rajoute 
+        // à la fin de la variable result qui correspond à l'affichage
         addNumber(nb) {
             this.result = ''+this.result +'' + nb;
         },
+
+        // Fonction prenant en paramètre l'opération' sélectioné par l'utilisateur, le garde en mémoire,
+        // stocke result dans resultmemory et supprime la valeur de result
         operation(op) {
             if (this.result != 0) {
                 this.operationmemory = op;
@@ -19,11 +24,14 @@ let app = new Vue({
                 this.result = '';
             }
         },
+
+        // Fonction prenant en compte l'opération et les valeurs sélectionnée par l'utilisateur, effectue le calcul,
+        // stocke sa valeur dans result et réinitialise resultmemory et operationmemory
         calcul() {
             if (this.operationmemory === '+') {
-                this.result = parseFloat(this.result) +parseFloat(this.resultmemory);
+                this.result = parseFloat(this.result) + parseFloat(this.resultmemory);
             } else if (this.operationmemory === '-') {
-                this.result = parseFloat(this.result) - parseFloat(this.resultmemory);
+                this.result =  parseFloat(this.resultmemory) - parseFloat(this.result);
             } else if (this.operationmemory === '*') {
                 this.result = parseFloat(this.result) * parseFloat(this.resultmemory);
             } else if (this.operationmemory === '/') {
@@ -33,11 +41,19 @@ let app = new Vue({
             this.operationmemory = '';
             this.resultmemory = '';
         },
+
+        plusless() {
+            this.result = this.result / 2;
+        },
+
+        // Réinitialise result, resultmemory et operationmemory
         clear() {
             this.result = '0';
             this.resultmemory = '';
             this.operationmemory = '';
         },
+
+        // Fonction rajoutant 2 zeros après la virgule
         purcent() {
             this.result = this.result.replace('.','');
             this.result = '0.0' + this.result;
